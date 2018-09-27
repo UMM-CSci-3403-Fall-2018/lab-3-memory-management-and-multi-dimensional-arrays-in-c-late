ce617271873915444ddcba87454c8be5edbb35d1
stev0531 <stev0531@morris.umn.edu>
2018-09-27 11:30:11 -0500

    Create array_merge.c
    We started creating array_merge.c.
    We have a skeleton that does not do anything yet.

---
+++ array_merge/array_merge.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "array_merge.h"
+
+bool isDouble(int value1,int value2){
+
+}
+
+
+int* array_merge(int num_arrays, int* sizes, int** values){
+  
+}
